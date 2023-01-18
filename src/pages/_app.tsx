c62ec64338a07182439acc14c6cbeae808d0a393
emilicos <notalvaroaustin@gmail.com>
2023-01-18 11:11:40 +0700

    feat: add footer and header for better SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Footer } from '@elements'
 
+const meta = {
+  title: "Alvaro's Portofolio",
+  description:
+    'Alvaro Austin personal website porfolio for showcasing experience and projects.',
+}
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  )
 }
